9a9341f7f7237146b93b40b277f6b58f814312e7
Raphael Morsomme <rmorsomme@gmail.com>
2019-01-12 13:06:25 +0100

    topic modeling (cluster level)

--- Shiny App.R
+++ Shiny App.R
@@ -552,6 +552,94 @@ server <- function(input, output) {
     
   })
   
+  #
+  # Modeling B: beta
+  output$modelingB_beta_title <- renderText({
+    
+    "Main Terms of each Topic"
+    
+  })
+  output$modelingB_beta_plot  <- renderPlot({
+    
+    model <- get(input$modelingB_ntopic)
+    
+    tidy(model, matrix = "beta") %>%
+      mutate(topic = paste("Topic", topic)) %>%
+      group_by(topic) %>%
+      top_n(10, beta) %>%
+      ungroup %>%
+      
+      ggplot(aes(x = reorder_within(term, by = beta, within = topic), y = beta, fill = topic)) +
+      geom_col(show.legend = F) +
+      facet_wrap(~ topic, scales = "free") +
+      scale_x_reordered() +
+      labs(x = "Terms", y = "Beta distribution") +
+      coord_flip() +
+      theme_light() +
+      theme(plot.title = element_text(hjust = 0.5))
+    
+  })
+  
+  # Modeling B: gamma1
+  output$modelingB_gamma1_title <- renderText({
+    
+    "Main Clusters of each Topic"
+    
+  })
+  output$modelingB_gamma1_plot  <- renderPlot({
+    
+    model   <- get(input$modelingB_ntopic)
+    
+    tidy(model, matrix = "gamma") %>%
+      left_join(d_course, by = c("document" = "Code")) %>%
+      mutate(topic = paste("Topic", topic)) %>%
+      filter(Cluster %in% input$modelingB_cluster,
+             gamma > 0.05) %>%
+      rename(facet = Cluster) %>%
+      group_by(facet, topic) %>%
+      summarise(gamma = sum(gamma)) %>%
+      filter(gamma > 0.05) %>%
+      ungroup %>%
+      
+      ggplot(aes(reorder_within(facet, by = gamma, within = topic), y = gamma, fill = topic)) +
+      geom_col(show.legend = F) +
+      facet_wrap(~ topic, scales = "free") +
+      scale_x_reordered() +
+      coord_flip() +
+      labs(x = NULL, y = "Gamma Distribution") +
+      theme_light() +
+      theme(plot.title = element_text(hjust = 0.5))
+    
+  })
+  
+  # Modeling B: gamma2
+  output$modelingB_gamma2_title <- renderText({
+    
+    "Main Topics per Cluster"
+    
+  })
+  output$modelingB_gamma2_plot  <- renderPlot({
+    
+    model   <- get(input$modelingA_ntopic)
+    
+    tidy(model, matrix = "gamma") %>%
+      left_join(d_course, by = c("document" = "Code")) %>%
+      mutate(topic = paste("Topic", topic)) %>%
+      filter(`Course Title` %in% input$modelingA_course,
+             gamma > 0.05) %>%
+      rename(facet = Title_short) %>%
+      
+      ggplot(aes(reorder_within(topic, by = gamma, within = facet), y = gamma, fill = topic)) +
+      geom_col(show.legend = F) +
+      facet_wrap(~ facet, scales = "free") +
+      scale_x_reordered() +
+      coord_flip() +
+      labs(x = NULL, y = "Gamma") +
+      theme_light() +
+      theme(plot.title = element_text(hjust = 0.5))
+    
+  })
+  
 }
 
 # Create Shiny app ####
